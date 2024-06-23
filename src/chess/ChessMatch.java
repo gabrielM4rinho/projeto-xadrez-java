16b475e5a9611612975b0906d549260c0ce8e838
Gabriel Marinho <gabriel.marinho.silva@outlook.com>
2024-06-23 19:23:11 -0300

    Movimentos das peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,7 +43,10 @@ public class ChessMatch {
 
     private void validateSourcePosition (Position position){
         if(!board.thereIsAPiece(position)){
-            throw new ChessException("Position not on the board");
+            throw new ChessException("There is no piece on source position");
+        }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible move for the chosen piece");
         }
     }
 
